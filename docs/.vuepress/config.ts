c016cac2acbac8ebfac1b893625cee0778b1bf98
Kunanon Srisuntiroj <son9912@gmail.com>
2022-06-02 11:53:05 +0000

    Applies head to website

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,29 +2,14 @@ import { defineUserConfig } from "@vuepress/cli";
 import { defaultTheme } from "@vuepress/theme-default";
 import { googleAnalyticsPlugin } from "@vuepress/plugin-google-analytics";
 import { docsearchPlugin } from "@vuepress/plugin-docsearch";
-import { navbar, sidebar } from "./configs";
+import { header, navbar, sidebar } from "./configs";
 
 export default defineUserConfig({
   // set site base to default value
   base: "/",
 
   // extra tags in `<head>`
-  head: [
-    [
-      "link",
-      {
-        rel: "stylesheet",
-        href: "https://cdn.jsdelivr.net/github-markdown-css/2.2.1/github-markdown.css",
-      },
-    ],
-    [
-      "link",
-      {
-        rel: "icon",
-        href: "https://img.icons8.com/fluency/32/000000/layers.png",
-      },
-    ],
-  ],
+  head: header,
 
   locales: {
     "/": {
---
+++ docs/.vuepress/configs/header.ts
@@ -0,0 +1,35 @@
+import type { HeadConfig } from "@vuepress/core";
+
+export const header: HeadConfig[] = [
+  [
+    "link",
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "https://img.icons8.com/fluency/16/000000/layers.png",
+    },
+  ],
+  [
+    "link",
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "https://img.icons8.com/fluency/32/000000/layers.png",
+    },
+  ],
+  [
+    "link",
+    {
+      rel: "stylesheet",
+      href: "https://cdn.jsdelivr.net/github-markdown-css/2.2.1/github-markdown.css",
+    },
+  ],
+  //   ["meta", { name: "application-name", content: "sagelga's Documentation" }],
+  //   [
+  //     "meta",
+  //     { name: "apple-mobile-web-app-title", content: "sagelga's Documentation" },
+  //   ],
+  //   ["meta", { name: "apple-mobile-web-app-status-bar-style", content: "black" }],
+];
